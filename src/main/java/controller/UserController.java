b65cb166b53a01014d2464c12d16b9c22b2372d2
Gringo911 <irbisaltai@yandex.ua>
2020-10-10 01:26:38 +0300

    four

---
+++ src/main/java/controller/UserController.java
@@ -0,0 +1,17 @@
+package controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/")
+public class UserController {
+
+    @GetMapping("/")
+    public String index() {
+
+        return "index";
+    }
+
+}
